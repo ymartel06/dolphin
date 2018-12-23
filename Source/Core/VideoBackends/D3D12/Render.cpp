71642505e19fe8a4ff12a47c918f1343de4ad954
Yohann Martel <yohann.martel@bhvr.com>
2018-12-23 14:49:32 -0500

    Add first d3d12 objects

--- Source/Core/VideoBackends/D3D12/Render.cpp
+++ Source/Core/VideoBackends/D3D12/Render.cpp
@@ -14,4 +14,41 @@ Renderer::~Renderer()
 
 }
 
+void Renderer::CreateRtvAndDsvDescriptorHeaps()
+{
+  /*
+  D3D12_COMMAND_QUEUE_DESC queueDesc = {};
+  queueDesc.Type = D3D12_COMMAND_LIST_TYPE_DIRECT;
+  queueDesc.Flags = D3D12_COMMAND_QUEUE_FLAG_NONE;
+  ThrowIfFailed(md3dDevice->CreateCommandQueue(&queueDesc, IID_PPV_ARGS(&mCommandQueue)));
+
+  ThrowIfFailed(md3dDevice->CreateCommandAllocator(
+      D3D12_COMMAND_LIST_TYPE_DIRECT, IID_PPV_ARGS(mDirectCmdListAlloc.GetAddressOf())));
+
+  ThrowIfFailed(
+      md3dDevice->CreateCommandList(0, D3D12_COMMAND_LIST_TYPE_DIRECT,
+                                    mDirectCmdListAlloc.Get(),  // Associated command allocator
+                                    nullptr,                    // Initial PipelineStateObject
+                                    IID_PPV_ARGS(mCommandList.GetAddressOf())));
+
+  // Start off in a closed state.  This is because the first time we refer
+  // to the command list we will Reset it, and it needs to be closed before
+  // calling Reset.
+  mCommandList->Close();
+  */
+}
+
+void Renderer::CreateCommandObjects()
+{
+}
+
+void Renderer::CreateSwapChain()
+{
+}
+
+bool Renderer::InitDirect3D()
+{
+  return false;
+}
+
 }  // namespace DX12
--- Source/Core/VideoBackends/D3D12/Render.h
+++ Source/Core/VideoBackends/D3D12/Render.h
@@ -4,6 +4,17 @@
 
 #pragma once
 
+#if defined(DEBUG) || defined(_DEBUG)
+#define _CRTDBG_MAP_ALLOC
+#include <crtdbg.h>
+#endif
+
+// Windows Runtime Library. Needed for Microsoft::WRL::ComPtr<> template class.
+#include <wrl.h>
+using namespace Microsoft::WRL;
+
+#include <d3d12.h>
+#include <dxgi1_6.h>
 #include "VideoCommon/RenderBase.h"
 
 namespace DX12
@@ -14,5 +25,32 @@ class Renderer : public ::Renderer
 public:
   Renderer(int backbuffer_width, int backbuffer_height);
   ~Renderer() override;
+
+private:
+  void CreateRtvAndDsvDescriptorHeaps();
+  void CreateCommandObjects();
+  void CreateSwapChain();
+  bool InitDirect3D();
+
+  ComPtr<IDXGIFactory4> m_dxgi_factory;
+  /*
+  Microsoft::WRL::ComPtr<IDXGISwapChain> m_swap_chain;
+  Microsoft::WRL::ComPtr<ID3D12Device> m_d3d_device;
+
+  Microsoft::WRL::ComPtr<ID3D12Fence> m_fence;
+  UINT64 m_current_fence = 0;
+
+  Microsoft::WRL::ComPtr<ID3D12CommandQueue> m_command_queue;
+  Microsoft::WRL::ComPtr<ID3D12CommandAllocator> m_direct_cmd_list_alloc;
+  Microsoft::WRL::ComPtr<ID3D12GraphicsCommandList> m_command_list;
+
+  static const int SwapChainBufferCount = 2;
+  int m_current_back_buffer = 0;
+  Microsoft::WRL::ComPtr<ID3D12Resource> m_swap_chain_buffer[SwapChainBufferCount];
+  Microsoft::WRL::ComPtr<ID3D12Resource> m_depth_stencil_buffer;
+
+  Microsoft::WRL::ComPtr<ID3D12DescriptorHeap> m_rtv_heap;
+  Microsoft::WRL::ComPtr<ID3D12DescriptorHeap> m_dsv_heap;
+  */
 };
 }  // namespace DX12
